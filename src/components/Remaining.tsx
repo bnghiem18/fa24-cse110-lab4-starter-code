44b68ebe13313f9f35565a8887e671e813ab932f
bnghiem18 <118479808+bnghiem18@users.noreply.github.com>
2024-10-26 12:01:11 -0700

    exercise 2 done

--- src/components/Remaining.tsx
+++ src/components/Remaining.tsx
@@ -12,11 +12,16 @@ const Remaining = () => {
   const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
 
   // Exercise: Create an alert when Remaining is less than 0.
-
+  const remaining = budget - totalExpenses;
+  if (remaining < 0){
+    alert("You have exceeded your budget!");  
+  }
+  
   return (
     <div className={`alert ${alertType}`}>
       <span>Remaining: ${budget - totalExpenses}</span>
     </div>
+  
   );
 };
 
